df656d769a2982a9ad0285a9ad0f63695bc42509
ddaeppo <ddaeppo@naver.com>
2025-08-27 23:49:46 +0900

    Update resource.ts groups 추가

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,10 +8,5 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-  userAttributes: {
-    preferredUsername: {
-      mutable: true,
-      required: false
-    }
-  }
+  groups: ['admin']
 });
