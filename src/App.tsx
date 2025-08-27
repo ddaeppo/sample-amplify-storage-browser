2465465c06bcf6bdf90b352ec0736a509a6690c8
ddaeppo <ddaeppo@naver.com>
2025-08-28 02:50:11 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       {({ signOut, user }) => (
         <>
           <div className="header">
-            <h1>{`Hello ${user?.username ?? "unknown"}`}</h1>
+            <h1>{`Hello ${user?.name ?? "unknown"}`}</h1>
             <Button onClick={signOut}>Sign out</Button>
           </div>
           <StorageBrowser />
