9d0fa487d30f9ec2b3b3af00f6eaef419c848a1d
ddaeppo <ddaeppo@naver.com>
2025-08-28 13:04:48 +0900

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -15,7 +15,7 @@ export const storage = defineStorage({
     'private/{entity_id}/*': [
         allow.entity('identity').to(['read', 'write', 'delete'])
     ],
-    'private/{cognito-identity.amazonaws.com:sub}/*': [
+    'private/{cognito-identity.amazonaws.com:sub}/upload/*': [
         allow.entity('identity').to(['read', 'write', 'delete'])
     ]
    })
